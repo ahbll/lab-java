a59c5ec6e64504930c4ab6d9887647784910a654
刘磊 <liulei3@bonc.com.cn>
2020-04-25 12:08:26 +0800

    init

---
+++ src/top/bhappy/Main.java
@@ -0,0 +1,12 @@
+package top.bhappy;
+
+/**
+ * @Author: liu lei
+ * @Date: 2020/4/25 12:07
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
